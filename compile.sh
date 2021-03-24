612f0bb68647d6a2030e747fb0ae7995f1a56e91
Paul Geppert <paul.geppert@student.hpi.de>
2021-03-24 12:03:34 +0100

    Improved compile via docker

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@
 docker build -t ns3-cv2x-compiler-better compile
 
 # Compile ns-3_c-v2x and launch a bash shell after that
-docker run --rm -it -v $(pwd):/ns3 ns3-cv2x-compiler-better bash
+docker run --rm --user $(id -u):$(id -g) -it -v $(pwd):/ns3 ns3-cv2x-compiler-better bash
--- compile/entrypoint.sh
+++ compile/entrypoint.sh
@@ -1,31 +1,9 @@
 #!/bin/sh
 
-git clone https://github.com/gjcarneiro/pybindgen /pybindgen/
-cd /pybindgen/
-git checkout 823d8b2b
-
-##############################################################################
-
-## Optional
-
-# Install this for --apiscan option
-
-# wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
-# cat get-pip.py | python
-# rm get-pip.py
-
-# apt install -y castxml
-# pip install cxxfilt
-# pip install pygccxml==1.9.0
-
-##############################################################################
-
-cd /ns3/
-
 ./waf clean
 ./waf distclean
 
 ./waf configure
-./waf build
+# ./waf build
 
 exec "$@"
