45b718bb5294333d988a55f4f289dbdce5a7f35e
Paul Geppert <paul.geppert@student.hpi.de>
2021-03-25 11:02:33 +0100

    Fixed build for python3

--- compile/entrypoint.sh
+++ compile/entrypoint.sh
@@ -7,6 +7,12 @@ python3 ./waf-2.0.22 clean
 python3 ./waf-2.0.22 distclean
 
 python3 ./waf-2.0.22 configure
-# python3 ./waf-2.0.22 build
+
+# First build will fail
+python3 ./waf-2.0.22 build
+# Because ns3module.cc is generated wrong
+cp fixed-lte-bindings-ns3module.cc build/src/lte/bindings/ns3module.cc
+# Build again
+python3 ./waf-2.0.22 build
 
 exec "$@"
