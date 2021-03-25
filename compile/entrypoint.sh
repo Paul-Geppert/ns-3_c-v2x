375b2d4ec4dd16719246ce490a35adbe3877f2c9
Paul Geppert <paul.geppert@student.hpi.de>
2021-03-25 11:02:22 +0100

    Correct waf version for Python3

--- compile/entrypoint.sh
+++ compile/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-./waf clean
-./waf distclean
+wget https://waf.io/waf-2.0.22
+chmod +x waf-2.0.22
 
-./waf configure
-# ./waf build
+python3 ./waf-2.0.22 clean
+python3 ./waf-2.0.22 distclean
+
+python3 ./waf-2.0.22 configure
+# python3 ./waf-2.0.22 build
 
 exec "$@"
