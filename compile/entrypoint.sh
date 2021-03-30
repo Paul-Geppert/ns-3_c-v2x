faa5a8359a4321148569ab6a69810a240f70c0ea
Paul Geppert <paul.geppert@student.hpi.de>
2021-03-30 12:10:31 +0000

    Use hardcoded pybindgen location

--- compile/entrypoint.sh
+++ compile/entrypoint.sh
@@ -5,7 +5,7 @@ chmod +x waf-2.0.22
 python3 ./waf-2.0.22 clean
 python3 ./waf-2.0.22 distclean
 
-python3 ./waf-2.0.22 configure
+python3 ./waf-2.0.22 configure --with-pybindgen=/pybindgen/
 
 # First build will fail
 python3 ./waf-2.0.22 build
