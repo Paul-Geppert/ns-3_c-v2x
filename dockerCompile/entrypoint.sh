c3daf279c1d5dd48aad393e8715bc0e0bf6a7365
Paul Geppert <paul.geppert@student.hpi.de>
2022-04-20 15:12:38 +0200

    Fixed error in build entrypoint

--- dockerCompile/entrypoint.sh
+++ dockerCompile/entrypoint.sh
@@ -14,7 +14,7 @@ cp fixed-lte-bindings-ns3module.cc build/src/lte/bindings/ns3module.cc
 
 # Also copy adapted version of network bindings
 # as it contains additional bindings
-cp fixed-network-bindings-ns3module.cc build/src/lte/bindings/ns3module.cc
+cp fixed-network-bindings-ns3module.cc build/src/network/bindings/ns3module.cc
 # Build again
 python3 ./waf-2.0.23 build
 
